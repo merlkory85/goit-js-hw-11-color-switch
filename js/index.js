4cd01b24ba90a5ff2de28b08582a747dbd77c6cc
Serhii Yavorskyi <merlkory1985@gmail.com>
2020-05-29 13:44:36 +0300

    make some change

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ const refs = {
 	startBtn: document.querySelector('button[data-action="start"'),
 	stopBtn: document.querySelector('button[data-action="stop"'),
 };
+
+let interval = null;
+let isActive = false;
+
 const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
 const randomIntegerFromInterval = (max) => {
 	return Math.floor(Math.random() * (max + 1));
@@ -12,11 +16,16 @@ const setRandomColor = () => {
 	document.body.style.backgroundColor = color;
 };
 
-let interval = null;
-
-refs.startBtn.addEventListener(
-	'click',
-	() => (interval = interval ? interval : setInterval(() => setRandomColor(), 1000))
-);
-
-refs.stopBtn.addEventListener('click', () => clearInterval(interval));
+refs.startBtn.addEventListener('click', () => {
+	if (isActive) {
+		return;
+	}
+	isActive = true;
+	interval = setInterval(() => {
+		setRandomColor();
+	}, 1000);
+});
+refs.stopBtn.addEventListener('click', () => {
+	clearInterval(interval);
+	isActive = false;
+});
